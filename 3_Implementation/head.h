78f0280d09b3f805b31b6edf320d09f772ebe752
AkhilPulicheri <94283305+AkhilPulicheri@users.noreply.github.com>
2021-11-23 19:44:37 +0530

    Update head.h

--- 3_Implementation/head.h
+++ 3_Implementation/head.h
@@ -1,3 +1,14 @@
+/**
+ * @file game.h
+ * @author Akhil Pulicheri
+ * @brief A program to play tictoctoe game.
+ * @version 0.1
+ * @date 2021-11-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 void layout_frame(char *cont[3][3]);
 void update_value(char input_number, int player, char *cont[3][3]);
 int check_winner(char *cont[3][3]);
